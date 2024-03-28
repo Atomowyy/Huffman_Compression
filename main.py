d83dfcd64dffe0d481509e3dc0063b0f9e6cd96d
Wojciech Lewko <wojciech.lewkoo@gmail.com>
2024-03-28 17:14:43 +0100

    Reading chars from file

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def read_file(filename: str) -> str:
+import math
+
+
+def update_file_informations(file) -> dict:
+    K: int = 0
+    unique_chars: set = set()
+    for line in file:
+        for char in line:
+            K += 1
+            unique_chars.add(char)
+
+    X: int = len(unique_chars)
+    N: int = math.ceil(math.log(X, 2))
+    #R need to be added
+
+    print(unique_chars)
+    print(X)
+    print(K)
+    print(N)
+    return None
+
+
+def huffman(file) -> None:
     pass
 
 
-text: str = read_file('')
-print(text)
+file_to_compress = open('do_kompresji.txt', 'r')
+update_file_informations(file_to_compress)
+file_to_compress.close()
