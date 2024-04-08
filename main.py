1905df418fa5cf544ded020c06d9823608cab435
Wojciech Lewko <wojciech.lewkoo@gmail.com>
2024-04-08 14:37:20 +0200

    update

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import math
 
 
-def update_file_informations(file) -> dict:
-    K: int = 0
+def update_file_informations(file) -> list:
+    file_informations: list = []
+    k: int = 0
     unique_chars: set = set()
     for line in file:
         for char in line:
-            K += 1
+            k += 1
             unique_chars.add(char)
 
-    X: int = len(unique_chars)
-    N: int = math.ceil(math.log(X, 2))
-    #R need to be added
-
-    print(unique_chars)
-    print(X)
-    print(K)
-    print(N)
+    x: int = len(unique_chars)
+    n: int = math.ceil(math.log(x, 2))
+    r: int = (8 - (3 + (k * n)) % 8) % 8
+    print(unique_chars)  # list of unique chars found in file to compress
+    print(x)  # count of unique_chars elements
+    print(k)  # count of chars in file to compress
+    print(n)  # code lenght
+    print(r)  # count of '0' to add at end of the file
+    dictionary = create_dictionary(unique_chars, n)
+    """file_informations.append()"""
     return None
+
+
+def create_dictionary(uniq_chars: set, n: int) -> dict:
+    coded_chars: dict = {}
+    print(f'List of unique chars: {uniq_chars}, code lenght for single char is: {n}')
+    for i in uniq_chars:
+        print(i)
+
+    return coded_chars
 
 
 def huffman(file) -> None:
