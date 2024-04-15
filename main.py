808a6069fd9eca599b8de866dc6cc6996fa77c6a
Wojciech Lewko <wojciech.lewkoo@gmail.com>
2024-04-15 08:43:38 +0200

    nie działa

--- main.py
+++ main.py
@@ -37,17 +37,15 @@ def create_dictionary(uniq_chars: set, n: int) -> dict:
 
 
 def huffman(file, file_informations: list) -> None:
-    compressed_file = open('skompresowane.txt', 'wb')
+    compressed_file = open('skompresowany.txt', 'wb')
     dictionary: dict = file_informations[0]
     chars_left: int = file_informations[1]
-
     compressed_file.write(len(dictionary).to_bytes(1, 'big'))
 
     for i in dictionary:
         compressed_file.write(i.to_bytes(1, 'big'))
 
     bin_str: str = ''
-
     chars_left_bin: str = bin(chars_left)[2:]
 
     while len(chars_left_bin) < 3:
@@ -74,7 +72,6 @@ def huffman(file, file_informations: list) -> None:
         bin_str += '0'
 
     compressed_file.write(int(bin_str, 2).to_bytes(1, 'big'))
-
     compressed_file.close()
 
 
@@ -93,53 +90,57 @@ def decompress_dict(chars_list: list) -> dict:
     return dictionary
 
 
-def dec_file_info(file) -> list:
+def dec_file(file) -> None:
     dictionary: dict = {}
     dict_lenght: int = 0
     end_bytes: list = []
     chars_list: list = []
     bin_char: str = ''
+
+    file_decompress = open('zdekompresowany.txt', 'w')
+
     for line in file:
         for i in line:
             if len(bin_char) != 0:
-                print(bin_char)
+                # print(bin_char)
                 if len(end_bytes) == 0:
-                    print(int(bin_char[:3], 2))
                     end_bytes.append(int(bin_char[:3], 2))
                     bin_char = bin_char[3:]
+                while len(bin_char) >= 2:
+                    decoded_char: str = bin_char[:2]
+                    file_decompress.write(dictionary[decoded_char])
 
-
-
-
-            # słownik
             if dict_lenght != 0 and len(chars_list) < dict_lenght:
                 chars_list.append(i)
                 if len(chars_list) == dict_lenght:
                     dictionary = decompress_dict(chars_list)
                     continue
-            # pierwszy znak kodu - ilość znaków w słowniku
+
             if dict_lenght == 0:
                 dict_lenght = i
 
             if len(dictionary) == dict_lenght:
-                bin_char: str = str(bin(i))[2:]
-                while len(bin_char) < 8:
-                    bin_char = '0' + bin_char
+                new_byte: str = str(bin(i))[2:]
+                while len(new_byte) < 8:
+                    new_byte = '0' + new_byte
+
+                bin_char = bin_char + new_byte
+
+    while len(bin_char) > end_bytes[0]:
+        decoded_char = bin_char[:2]
+        file_decompress.write(dictionary[decoded_char])
+        bin_char = bin_char[2:]
+
+    file_decompress.close()
 
 
-    print(end_bytes)
-    print(dictionary)
+#file_to_compress = open('do_kompresji.txt', 'rb')
+#file_informations_list: list = update_file_informations(file_to_compress)
+#file_to_compress.close()
+#file_to_compress = open('do_kompresji.txt', 'rb')
+#huffman(file_to_compress, file_informations_list)
+#file_to_compress.close()
 
+file_to_decompress = open('skompresowany.txt', 'rb')
+dec_file(file_to_decompress)
 
-# file_to_compress = open('do_kompresji.txt', 'rb')
-# file_informations_list: list = update_file_informations(file_to_compress)
-# file_to_compress.close()
-
-# file_to_compress = open('do_kompresji.txt', 'rb')
-# huffman(file_to_compress, file_informations_list)
-# file_to_compress.close()
-
-
-file_to_decompress = open('skompresowane.txt', 'rb')
-
-dec_file_info(file_to_decompress)
